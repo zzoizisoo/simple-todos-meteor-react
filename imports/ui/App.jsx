3112d696ad0c76ed402e9edb171988aa42ceb645
zzoizisoo <zzoizisoo@gmail.com>
2024-03-28 21:10:55 +0900

    2.3: Render Tasks Cllection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data'
+import { TasksCollection } from '../api/TasksCollection.js';
 import { Task } from './Task.jsx';
 
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
 
-    <ul>
-      {tasks.map(task => <Task key={task._id} task={task}/>)}
-    </ul>
-    
-    {/* <Hello/>
-    <Info/>
-     */}
-  </div>
-);
+export const App = () => {
+  const tasks = useTracker(()=> TasksCollection.find({}).fetch())
+
+  return(
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <ul>
+        {tasks.map(task => <Task key={task._id} task={task}/>)}
+      </ul>
+      
+      {/* <Hello/>
+      <Info/>
+      */}
+    </div>
+  )
+};
