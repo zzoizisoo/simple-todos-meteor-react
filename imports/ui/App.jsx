1c8f7bf00e328ecf940874ac1d923b8a621da9dd
Jisu Choi <zzoi.zisoo@gmail.com>
2024-03-29 19:07:16 +0900

    3.5: Show Newest Tasks First

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,18 +7,19 @@ import { TaskForm } from './TaskForm.jsx';
 
 
 export const App = () => {
-  const tasks = useTracker(()=> TasksCollection.find({}).fetch())
+  // const tasks = useTracker(()=> TasksCollection.find({}).fetch())
+  const tasks = useTracker(() => TasksCollection.find({},{ sort: { createdAt: -1 }}).fetch())
 
-  return(
+  return (
     <div>
       <h1>Welcome to Meteor!</h1>
 
       <TaskForm />
 
       <ul>
-        {tasks.map(task => <Task key={task._id} task={task}/>)}
+        {tasks.map(task => <Task key={task._id} task={task} />)}
       </ul>
-      
+
       {/* <Hello/>
       <Info/>
       */}
--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,16 +1,34 @@
 import React, {useState} from "react";
+import { TasksCollection } from "../api/TasksCollection";
 
 export const TaskForm = () => { 
     const [text, setText] = useState('')
+
+    const handleSubmit = (e) => {
+        console.log('new task submit click')
+        e.preventDefault();
+
+        if(!text) return;
+
+        TasksCollection.insert({
+            text: text.trim(),
+            createdAt: new Date()
+        })
+
+        setText("")
+    }
+
 
     return(
         <form className="task-form"> 
             <input
                 type="text"
                 placeholder="새 할일을 입력하세요"
+                value={text}
+                onChange={e => setText(e.target.value)}
             />
 
-            <button type="submit">할 일 추가</button>
+            <button type="submit" onClick={handleSubmit}>할 일 추가</button>
         </form>
     )
 }
