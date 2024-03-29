196f7b18090fbdcba59a11007420af1fb107f945
Jisu Choi <zzoi.zisoo@gmail.com>
2024-03-29 18:55:29 +0900

    3.2: Update the App component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data'
 import { TasksCollection } from '../api/TasksCollection.js';
 import { Task } from './Task.jsx';
-
+import { TaskForm } from './TaskForm.jsx';
 
 
 
@@ -12,6 +12,8 @@ export const App = () => {
   return(
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
 
       <ul>
         {tasks.map(task => <Task key={task._id} task={task}/>)}
