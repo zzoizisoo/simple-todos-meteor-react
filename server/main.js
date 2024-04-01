a8273f3256b20f19fd21a2407de8667503971800
zzoizisoo <zzoizisoo@gmail.com>
2024-04-01 21:41:40 +0900

    7.2: Create User Account

--- server/main.js
+++ server/main.js
@@ -1,10 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '../imports/api/TasksCollection';
+
+const SEED_USERNAME = 'jisuchoi';
+const SEED_PASSWORD = 'password';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText })
 
 
 Meteor.startup(async () => {
+  if(!Accounts.findUserByUsername(SEED_USERNAME)){ 
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD
+    })
+  }
   if(TasksCollection.find().count() ===0){ 
     [
       'First',
